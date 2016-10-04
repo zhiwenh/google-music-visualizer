56cdee51d20233b8bca1502aa811e72caa6b2b49
zhiwenhuang <zhiwen555@gmail.com>
2016-10-03 19:10:46 -0700

    Added a mode button visualizer bar and fixed issue where extension would not start

--- scripts/content.js
+++ scripts/content.js
@@ -3,263 +3,323 @@
 // imports jquery
 
 window.onload = function() {
-  // VISUALIZER INITIALIZATION AND RENDER START
-  var audioElement = $('audio')[0];
-  console.log(audioElement);
-  var visualizer = new AudioVisualizer();
-  visualizer.create(audioElement, '#player');
 
-  // refer to visualizer for options meaning
-  visualizer.analyserOptions({
-    fftSize: 2048,
-    minDecibels: -87,
-    maxDecibels: -3,
-    smoothingTimeConstant: 0.83
-  });
+  /** Looks for audio element after a second then intervals of half seconds */
+  setTimeout(function() {
+    var findAudioInterval = setInterval(function() {
+      findAudio();
+    }, 500);
+    findAudio();
 
-  visualizer.containerStyles({
-    position: 'absolute',
-    top: visualizer.containerHeight * -1,
-    left: 0,
-    'z-index': 10000,
-    'pointer-events': 'none'
-  });
+    function findAudio() {
+      var $audio = $('audio');
 
-  visualizer.options({
-    color: 'orange',
-    opacity: 0.7,
-    interval: 30,
-    frequencyDataDivide: 9,
-    barPadding: 1.7
-  });
-
-  visualizer.initialize();
-  visualizer.start();
-
-
-  /* ################################### */
-
-  // GOOGLE MUSIC BUTTON DISPLAY
-  var buttonContainer = $('<div></div>');
-  buttonContainer.appendTo('#player');
-  buttonContainer.css({
-    display: 'flex',
-    flexFlow: 'row',
-    height: '22px',
-    position: 'absolute',
-    top: '67px',
-    left: '100px',
-    zIndex: 10000,
-    border: '1px solid #d3d3d3',
-    outline: 'none',
-    textAlign: 'center',
-    boxSizing: 'border-box',
-    color: '#FB7B62', // initial color of display words. changes with colorArr
-    backgroundColor: '#FB7B62',
-    fontFamily: 'Roboto, arial',
-    fontSize: '13px',
-    userSelect: 'none', // no text highlighting
-    borderRadius: '2px'
-  });
-
-  var cssDefaults = {
-    textAlign: 'center',
-    boxSizing: 'border-box',
-    backgroundColor: 'white',
-    height: '100%',
-    outline: 'none',
-    cursor: 'pointer',
-  };
-  // NAME
-  var visualizerName = $('<div>visualization</div>');
-  visualizerName.appendTo(buttonContainer);
-  visualizerName.css(cssDefaults);
-  visualizerName.css({
-    paddingLeft: '5px',
-    paddingTop: '2px',
-    paddingRight: '3px',
-  });
-
-  // COLOR CHANGE BUTTON
-  var colorChange = $('<div>orange</div>');
-  colorChange.appendTo(buttonContainer);
-  colorChange.css(cssDefaults);
-  colorChange.css({
-    width: '65px',
-    paddingTop: '2px',
-    marginRight: '22px',
-    borderRight: '1px solid #d3d3d3',
-  });
-
-  // SPEED BUTTONS
-  var speedButtons = [];
-  for (var i = 0; i < 3; i++) {
-    var speedButton;
-    if (i === 0) {
-      speedButton = $('<div>-</div>');
-    } else if (i === 2) {
-      speedButton = $('<div>+</div>');
-    } else {
-      speedButton = $('<div></div>');
+      if ($audio.length >= 1) {
+        clearInterval(findAudioInterval);
+        var audioElement = $audio[$audio.length - 1];
+        startUp(audioElement);
+      }
     }
-    speedButton.appendTo(buttonContainer);
-    speedButton.css(cssDefaults);
-    speedButton.css({
-      width: '30px',
-      paddingTop: '3px',
-      borderRight: '1px solid #d3d3d3',
-      borderLeft: '1px solid #d3d3d3'
+  }, 1000)
+
+  /** Called after audio element is found */
+  function startUp(audioElement) {
+    // VISUALIZER INITIALIZATION AND RENDER START
+    var visualizer = new AudioVisualizer();
+    visualizer.create(audioElement, '#player');
+
+    // refer to visualizer for options meaning
+    visualizer.analyserOptions({
+      fftSize: 2048,
+      minDecibels: -87,
+      maxDecibels: -3,
+      smoothingTimeConstant: 0.83
     });
-    speedButtons.push(speedButton);
-  }
-  speedButtons[1].text(Math.floor(1000 / visualizer.getInterval()) + ' Hz');
-  speedButtons[1].css({
-    cursor: 'default',
-    backgroundColor: 'white',
-    width: '47px'
-  });
-  speedButtons[2].css({
-    marginRight: '22px'
-  });
 
-  // HEIGHT BUTTONS
-  var heightButtons = [];
-  for (i = 0; i < 3; i++) {
-    var heightButton;
-    if (i === 0) {
-      heightButton = $('<div>-</div>');
-    } else if (i === 2) {
-      heightButton = $('<div>+</div>');
-    } else {
-      heightButton = $('<div></div>');
-    }
-    heightButton.appendTo(buttonContainer);
-    heightButton.css(cssDefaults);
-    heightButton.css({
-      width: '30px',
-      paddingTop: '3px',
-      borderRight: '1px solid #d3d3d3',
-      borderLeft: '1px solid #d3d3d3'
+    visualizer.containerStyles({
+      position: 'absolute',
+      top: visualizer.containerHeight * -1,
+      left: 0,
+      'z-index': 10000,
+      'pointer-events': 'none'
     });
-    heightButtons.push(heightButton);
-  }
-  heightButtons[1].text(visualizer.getBarHeightScale() + 'x');
-  heightButtons[1].css({
-    cursor: 'default',
-    backgroundColor: 'white',
-    width: '35px'
-  });
-  heightButtons[2].css({
-    marginRight: '22px'
-  });
 
-  // OFF visualizer BUTTON
-  var off = $('<div>off</div>');
-  off.appendTo(buttonContainer);
-  off.css(cssDefaults);
-  off.css({
-    width: '35px',
-    paddingTop: '2px',
-    paddingLeft: '5px',
-    paddingRight: '5px',
-    borderLeft: '1px solid #d3d3d3',
-  });
+    visualizer.options({
+      color: 'orange',
+      opacity: 0.7,
+      interval: 30,
+      frequencyDataDivide: 9,
+      barPadding: 1.7
+    });
 
+    visualizer.initialize();
+    visualizer.start();
 
+    /* ################################### */
+    // GOOGLE MUSIC BUTTON DISPLAY
+    /* ################################### */
 
-  // array of all the colors in AudioVisualizer. iterate thru this with each
-  // click to display a different color
-  var colorsIndex = 0;
-  var colorsArr = [
-    ['green', '#4FD55A'],
-    ['blue', '#5CD1F8'],
-    ['purple', '#FB628E'],
-    ['red', '#D60401'],
-    ['gray', '#BDBDBD'],
-    ['orange', '#FF5722']
-  ];
-
-  // toggles the different color themes
-  function colorToggle() {
-    // change color of visualizer
-    visualizer.setColor(colorsArr[colorsIndex][0]);
-    // change text color of the display container and background color
+    var buttonContainer = $('<div></div>');
+    buttonContainer.appendTo('#player');
     buttonContainer.css({
-      color: colorsArr[colorsIndex][1],
-      backgroundColor: colorsArr[colorsIndex][1]
+      display: 'flex',
+      flexFlow: 'row',
+      height: '22px',
+      position: 'absolute',
+      top: '67px',
+      left: '100px',
+      zIndex: 10000,
+      border: '1px solid #d3d3d3',
+      outline: 'none',
+      textAlign: 'center',
+      boxSizing: 'border-box',
+      color: '#FB7B62', // initial color of display words. changes with colorArr
+      backgroundColor: '#FB7B62',
+      fontFamily: 'Roboto, arial',
+      fontSize: '13px',
+      userSelect: 'none', // no text highlighting
+      borderRadius: '2px'
     });
-    // change color of google music progress bar
-    $('#primaryProgress').css({
-      background: colorsArr[colorsIndex][1]
+
+    var cssDefaults = {
+      textAlign: 'center',
+      boxSizing: 'border-box',
+      backgroundColor: 'white',
+      height: '100%',
+      outline: 'none',
+      cursor: 'pointer',
+    };
+
+    /* ################################### */
+    // NAME
+    /* ################################### */
+    var visualizerName = $('<div>visualization</div>');
+    visualizerName.appendTo(buttonContainer);
+    visualizerName.css(cssDefaults);
+    visualizerName.css({
+      paddingLeft: '5px',
+      paddingTop: '2px',
+      paddingRight: '3px',
     });
-    $('#sliderKnobInner').css({
-      backgroundColor: colorsArr[colorsIndex][1],
-      borderColor: colorsArr[colorsIndex][1]
+
+    /* ################################### */
+    // COLOR CHANGE BUTTON
+    /* ################################### */
+    var colorChange = $('<div>orange</div>');
+    colorChange.appendTo(buttonContainer);
+    colorChange.css(cssDefaults);
+    colorChange.css({
+      width: '65px',
+      paddingTop: '2px',
+      marginRight: '22px',
+      borderRight: '1px solid #d3d3d3',
     });
-    // change color of icon
-    $('#player-bar-play-pause').css({
-      color: colorsArr[colorsIndex][1]
-    });
-    // change color of top toolbar
-    $('#material-app-bar').css({
-      background: colorsArr[colorsIndex][1]
-    });
-    colorChange.text(colorsArr[colorsIndex][0]);
-    if (colorsIndex === colorsArr.length - 1) {
-      colorsIndex = 0;
-    } else {
-      colorsIndex++;
+
+    /* ################################### */
+    // SPEED BUTTONS
+    /* ################################### */
+    var speedButtons = [];
+    for (var i = 0; i < 3; i++) {
+      var speedButton;
+      if (i === 0) {
+        speedButton = $('<div>-</div>');
+      } else if (i === 2) {
+        speedButton = $('<div>+</div>');
+      } else {
+        speedButton = $('<div></div>');
+      }
+      speedButton.appendTo(buttonContainer);
+      speedButton.css(cssDefaults);
+      speedButton.css({
+        width: '30px',
+        paddingTop: '3px',
+        borderRight: '1px solid #d3d3d3',
+        borderLeft: '1px solid #d3d3d3'
+      });
+      speedButtons.push(speedButton);
     }
+    speedButtons[1].text(Math.floor(1000 / visualizer.getInterval()) + ' Hz');
+    speedButtons[1].css({
+      cursor: 'default',
+      backgroundColor: 'white',
+      width: '47px'
+    });
+    speedButtons[2].css({
+      marginRight: '22px'
+    });
+
+    /* ################################### */
+    // HEIGHT BUTTONS
+    /* ################################### */
+    var heightButtons = [];
+    for (i = 0; i < 3; i++) {
+      var heightButton;
+      if (i === 0) {
+        heightButton = $('<div>-</div>');
+      } else if (i === 2) {
+        heightButton = $('<div>+</div>');
+      } else {
+        heightButton = $('<div></div>');
+      }
+      heightButton.appendTo(buttonContainer);
+      heightButton.css(cssDefaults);
+      heightButton.css({
+        width: '30px',
+        paddingTop: '3px',
+        borderRight: '1px solid #d3d3d3',
+        borderLeft: '1px solid #d3d3d3'
+      });
+      heightButtons.push(heightButton);
+    }
+    heightButtons[1].text(visualizer.getBarHeightScale() + 'x');
+    heightButtons[1].css({
+      cursor: 'default',
+      backgroundColor: 'white',
+      width: '35px'
+    });
+    heightButtons[2].css({
+      marginRight: '22px'
+    });
+
+    /* ################################### */
+    // MODE visualizer BUTTON
+    /* ################################### */
+    var mode = $('<div>mode</div>');
+    mode.appendTo(buttonContainer);
+    mode.css(cssDefaults);
+    mode.css({
+      width: '50px',
+      paddingTop: '2px',
+      paddingLeft: '5px',
+      paddingRight: '5px',
+      borderLeft: '1px solid #d3d3d3',
+    });
+
+    /* ################################### */
+    // OFF visualizer BUTTON
+    /* ################################### */
+    var off = $('<div>off</div>');
+    off.appendTo(buttonContainer);
+    off.css(cssDefaults);
+    off.css({
+      width: '35px',
+      paddingTop: '2px',
+      paddingLeft: '5px',
+      paddingRight: '5px',
+      borderLeft: '1px solid #d3d3d3',
+    });
+
+
+
+    // array of all the colors in AudioVisualizer. iterate thru this with each
+    // click to display a different color
+    var colorsIndex = 0;
+    var colorsArr = [
+      ['green', '#4FD55A'],
+      ['blue', '#5CD1F8'],
+      ['purple', '#FB628E'],
+      ['red', '#D60401'],
+      ['gray', '#BDBDBD'],
+      ['orange', '#FF5722']
+    ];
+
+    // toggles the different color themes
+    function colorToggle() {
+      // change color of visualizer
+      visualizer.setColor(colorsArr[colorsIndex][0]);
+      // change text color of the display container and background color
+      buttonContainer.css({
+        color: colorsArr[colorsIndex][1],
+        backgroundColor: colorsArr[colorsIndex][1]
+      });
+      // change color of google music progress bar
+      $('#primaryProgress').css({
+        background: colorsArr[colorsIndex][1]
+      });
+      $('#sliderKnobInner').css({
+        backgroundColor: colorsArr[colorsIndex][1],
+        borderColor: colorsArr[colorsIndex][1]
+      });
+      // change color of icon
+      $('#player-bar-play-pause').css({
+        color: colorsArr[colorsIndex][1]
+      });
+      // change color of top toolbar
+      $('#material-app-bar').css({
+        background: colorsArr[colorsIndex][1]
+      });
+      colorChange.text(colorsArr[colorsIndex][0]);
+      if (colorsIndex === colorsArr.length - 1) {
+        colorsIndex = 0;
+      } else {
+        colorsIndex++;
+      }
+    }
+
+    colorChange.on('click', colorToggle);
+    visualizerName.on('click', colorToggle);
+
+    // slow down
+    speedButtons[0].on('click', function() {
+      if (visualizer.getInterval() < 100) {
+        visualizer.slower(5);
+        speedButtons[1].text(Math.floor(1000 / visualizer.getInterval()) + ' Hz');
+      }
+    });
+    // speed up
+    speedButtons[2].on('click', function() {
+      if (visualizer.getInterval() > 5) {
+        visualizer.faster(5);
+        speedButtons[1].text(Math.floor(1000 / visualizer.getInterval()) + ' Hz');
+      }
+    });
+
+    // height decrease
+    heightButtons[0].on('click', function() {
+      var barHeightScale = visualizer.getBarHeightScale();
+      if (barHeightScale > 0.1) {
+        barHeightScale -= 0.1;
+        visualizer.setBarHeightScale(barHeightScale);
+        heightButtons[1].text(barHeightScale.toFixed(1)+ 'x');
+      }
+    });
+    // height increase
+    heightButtons[2].on('click', function() {
+      var barHeightScale = visualizer.getBarHeightScale();
+      if (barHeightScale < 2) {
+        barHeightScale += 0.1;
+        visualizer.setBarHeightScale(barHeightScale);
+        heightButtons[1].text(barHeightScale.toFixed(1) + 'x');
+      }
+    });
+
+    // ON/OFF
+    var isOn = false;
+    off.on('click', function() {
+      if (isOn === false) {
+        isOn = true;
+        visualizer.stop();
+        off.text('on');
+      } else {
+        isOn = false;
+        visualizer.start();
+        off.text('off');
+      }
+    });
+
+    var whatMode = 'player';
+    mode.on('click', function() {
+      $mainContainer = $('#mainContainer');
+      if (whatMode === 'player') {
+        $mainContainer.hide('fast');
+        whatMode = 'audio';
+      } else {
+        $mainContainer.show('fast');
+        whatMode = 'player';
+      }
+    })
   }
 
-  colorChange.on('click', colorToggle);
-  visualizerName.on('click', colorToggle);
 
-  // slow down
-  speedButtons[0].on('click', function() {
-    if (visualizer.getInterval() < 100) {
-      visualizer.slower(5);
-      speedButtons[1].text(Math.floor(1000 / visualizer.getInterval()) + ' Hz');
-    }
-  });
-  // speed up
-  speedButtons[2].on('click', function() {
-    if (visualizer.getInterval() > 5) {
-      visualizer.faster(5);
-      speedButtons[1].text(Math.floor(1000 / visualizer.getInterval()) + ' Hz');
-    }
-  });
 
-  // height decrease
-  heightButtons[0].on('click', function() {
-    var barHeightScale = visualizer.getBarHeightScale();
-    if (barHeightScale > 0.1) {
-      barHeightScale -= 0.1;
-      visualizer.setBarHeightScale(barHeightScale);
-      heightButtons[1].text(barHeightScale.toFixed(1)+ 'x');
-    }
-  });
-  // height increase
-  heightButtons[2].on('click', function() {
-    var barHeightScale = visualizer.getBarHeightScale();
-    if (barHeightScale < 2) {
-      barHeightScale += 0.1;
-      visualizer.setBarHeightScale(barHeightScale);
-      heightButtons[1].text(barHeightScale.toFixed(1) + 'x');
-    }
-  });
-
-  // ON/OFF
-  var isOn = false;
-  off.on('click', function() {
-    if (isOn === false) {
-      isOn = true;
-      visualizer.stop();
-      off.text('on');
-    } else {
-      isOn = false;
-      visualizer.start();
-      off.text('off');
-    }
-  });
 };
